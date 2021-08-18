ec591512f5c4c28bc07b6b6a0b2891d675a4dfce
LJH <740988959@qq.com>
2021-08-18 23:23:19 +0800

    最长回文子串

--- 100Liked/Medium/longestPalindrome.py
+++ 100Liked/Medium/longestPalindrome.py
@@ -1,5 +1,8 @@
 def longestPalindrome(s: str) -> str:
-    
+    # 找最长的回文子串，使用中心扩散法，首先定义一个扩散函数，从左右开始向两边扩散
+    # 终止条件是左右都越界
+    # 注意返回长度是right-left-1，因为到达最后一个判定之后，左右都又扩散了一次
+    # 这里还要返回左指针作为起始点
     def centerSpread(string, left, right):
         while left >= 0 and right < len(string):
             if string[left] == string[right]:
@@ -12,6 +15,7 @@ def longestPalindrome(s: str) -> str:
     max_len = 1
     max_left = 0
     for i in range(0, len(s)-1):
+        # 中心扩散分为奇数长度子串和偶数长度子串进行扩散
         odd_len, odd_left = centerSpread(s, i, i)
         even_len, even_left = centerSpread(s, i, i+1)
         if odd_len >= even_len:
